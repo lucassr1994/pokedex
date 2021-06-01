9a6da4e0e956da3b203cc63232fed08a7f3ddbfc
Lucas Silveira Ribeiro <lucassr1994@gmail.com>
2021-06-01 04:28:48 -0300

    drag and drop implemented

--- src/views/FirstPokemon/FirstPokemon.js
+++ src/views/FirstPokemon/FirstPokemon.js
@@ -4,21 +4,34 @@ import './FirstPokemon.css';
 
 export default function FirstPokemon() {
   const dragStartHandler = (event) => {
+    event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
   }
   const dragOverHandler = (event) => {
+    event.preventDefault()
   }
   const dropHandler = (event) => {
+    const id = event
+    .dataTransfer
+    .getData('text')
+    const draggableElement = document.getElementById(id)
+    const dropzone = event.target
+    dropzone.appendChild(draggableElement)
+    event
+    .dataTransfer
+    .clearData()
   }
   return (
     <div className="FirstPokemon-page">
       <div className="Pokemon-options">
-        <div draggable="true" onDragStart={dragStartHandler}>
+        <div id="draggable-1" draggable="true" onDragStart={dragStartHandler}>
           <PokeDexCard number={1} />
         </div>
-        <div draggable="true">
+        <div id="draggable-2" draggable="true" onDragStart={dragStartHandler}>
           <PokeDexCard number={4} />
         </div>
-        <div draggable="true">
+        <div id="draggable-3" draggable="true" onDragStart={dragStartHandler}> 
           <PokeDexCard number={7} />
         </div>
       </div>
